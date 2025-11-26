8ae837c8d333e2a7fba0d13b4fb0996f366eb5fb
marinadw452 <maaara62626@gmail.com>
2025-11-26 23:42:21 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-<?php
-$DATABASE_URL = getenv("DATABASE_URL");
-
-if (!$DATABASE_URL) {
-    die("❌ DATABASE_URL غير موجود.");
-}
-
-$conn = pg_connect($DATABASE_URL) or die("❌ فشل الاتصال: " . pg_last_error());
