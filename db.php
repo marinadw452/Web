8ab33cdcc6b8c81fe5a03af2e9cdd557643550ad
marinadw452 <maaara62626@gmail.com>
2025-11-30 15:25:29 +0300

    Delete db.php

--- db.php
+++
@@ -1,9 +0,0 @@
-$url = getenv("DATABASE_URL");
-$parts = parse_url($url);
-$host = $parts['host'];
-$port = $parts['port'];
-$user = $parts['user'];
-$pass = $parts['pass'];
-$dbname = ltrim($parts['path'], '/');
-
-$pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$pass");
