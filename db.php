2ec8b20ed0a497e249f6d61d464f28141040a654
marinadw452 <maaara62626@gmail.com>
2025-11-30 14:56:05 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,23 +1,9 @@
-<?php
-// db.php
 $url = getenv("DATABASE_URL");
+$parts = parse_url($url);
+$host = $parts['host'];
+$port = $parts['port'];
+$user = $parts['user'];
+$pass = $parts['pass'];
+$dbname = ltrim($parts['path'], '/');
 
-if ($url) {
-    $parts = parse_url($url);
-    $host = $parts['host'];
-    $port = $parts['port'];
-    $user = $parts['user'];
-    $pass = $parts['pass'];
-    $dbname = ltrim($parts['path'], '/');
-
-    try {
-        $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$pass");
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "✅ الاتصال ناجح!";
-    } catch (PDOException $e) {
-        die("❌ خطأ في الاتصال: " . $e->getMessage());
-    }
-} else {
-    die("❌ متغير DATABASE_URL غير موجود.");
-}
-?>
+$pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$pass");
