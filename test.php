63ebb251868c1746b166b0ca3f0e5fb5de69fde9
marinadw452 <maaara62626@gmail.com>
2025-12-01 15:23:17 +0300

    Delete test.php

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "db.php"; // لو حاط اتصالك في ملف آخر
-
-$result = pg_query($conn, "SELECT NOW()");
-$row = pg_fetch_assoc($result);
-
-echo "Database OK<br>";
-echo $row['now'];
